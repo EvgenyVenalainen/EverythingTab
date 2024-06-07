7c11a25aff25ea6c0613b57119b10366c27e5db4
Evgeny <eug_ptz@mail.ru>
2024-06-08 01:24:30 +0300

    Unnecessary file removing

--- rar.cmd
+++
@@ -1,4 +0,0 @@
-rem Берем имя родительского каталога без полного пути.
-for %%I in (.) do set CWD=%%~nI
-rem Архивируем.
-"C:\Program Files\WinRAR\WinRAR.exe" a -v -r -s -ag- -x*.rar -x*.7z -x*.obj -x*.obj -x*.idb -x*.aps -x*.ncb -x*.pdb -x*.bsc -x*.exp -x*.pch -x*.res -x*.ilk -x*.sbr -x\Debug*\*.* -x*Debug\*.exe -x\Debug*\*.dll %CWD%
